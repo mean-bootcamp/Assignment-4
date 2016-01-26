e22e5eb446931ee923a0a69bbf132e1a40da0586
jeffrey mclemore <jmclemore368@gmail.com>
2016-01-26 15:26:48 -0500

    Completed Assignment 4

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,10 @@
-var path = require('path'),  
-    express = require('express'), 
+var path = require('path'),
+    express = require('express'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes'), 
+    listingsRouter = require('../routes/listings.server.routes'),
     getCoordinates = require('../controllers/coordinates.server.controller.js');
 
 module.exports.init = function() {
@@ -17,7 +17,7 @@ module.exports.init = function() {
   //enable request logging for development debugging
   app.use(morgan('dev'));
 
-  //body parsing middleware 
+  //body parsing middleware
   app.use(bodyParser.json());
 
   /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
@@ -25,13 +25,27 @@ module.exports.init = function() {
     res.send(req.results);
   });
 
+
+
+
+
   /* serve static files */
-  
+  //See the following reference:
+  //http://expressjs.com/en/starter/static-files.html
+  app.use('/static', express.static('client'));
 
   /* use the listings router for requests to the api */
+  //Requests to the below URI will redirect to listingsRouter
+  app.use('/api/listings', listingsRouter);
 
-
-  /* go to homepage for all routes not specified */ 
+  /* go to homepage for all routes not specified */
+  //See the following reference for app.all
+  //http://expressjs.com/en/api.html
+  //The handler is executed for requests to any not specified
+  //(ie., “/*”) whether
+  // using GET, POST, PUT, DELETE, or any other HTTP request method.
+  app.all('/*', function(request, response, next) {
+    response.redirect('../index.html');});
 
   return app;
-};  
+};
--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,14 +1,14 @@
 
 /* Dependencies */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Listing = require('../models/listings.server.model.js');
 
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
-  On an error you should send a 404 status code, as well as the error message. 
+  On an error you should send a 404 status code, as well as the error message.
   On success (aka no error), you should send the listing(s) as JSON in the response.
 
-  HINT: if you are struggling with implementing these functions, refer back to this tutorial 
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
 
@@ -19,10 +19,11 @@ exports.create = function(req, res) {
   var listing = new Listing(req.body);
 
   /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
+  //We must also add a requirement for address as specified in the assignment.
+  if(req.results && req.results.address) {
     listing.coordinates = {
-      latitude: req.results.lat, 
-      longitude: req.results.lng
+      longitude: req.results.lng,
+      latitude: req.results.lat
     };
   }
 
@@ -32,6 +33,8 @@ exports.create = function(req, res) {
       console.log(err);
       res.status(400).send(err);
     } else {
+      //Else, Sends a JSON response composed of a stringified
+      //version of the specified data
       res.json(listing);
     }
   });
@@ -48,27 +51,79 @@ exports.update = function(req, res) {
   var listing = req.listing;
 
   /* Replace the article's properties with the new properties found in req.body */
+  listing.address = req.body.address;
+  listing.name = req.body.name;
+  listing.code = req.body.code;
+
   /* save the coordinates (located in req.results if there is an address property) */
+  //Same as above. Must add check that address exists.
+  if (req.results && req.results.address) {
+    listing.coordinates = {
+      latitude: req.results.lat,
+      longitude: req.results.lng
+    };
+  }
+
   /* Save the article */
+  listing.save(function(error) {
+    if (error) {
+      //Send 400 if error
+      res.status(400).send(error);
+    }
+    else {
+      //Else, Sends a JSON response composed of a stringified
+      //version of the specified data
+      res.json(listing);
+    }
+  });
 };
+
+
+
+
+
+
 
 /* Delete a listing */
 exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Remove the article */
-};
+  Listing.remove({code:listing.code}, function(error) {
+    if (error) {
+      //Send 400 if error
+      res.status(400).send(error);
+    }
+    else {
+      //Else, Sends a JSON response composed of a stringified
+      //version of the specified data
+      res.json(listing);
+    }});};
+
+
+
+
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
+
   /* Your code here */
-};
+  //Retrieve all listings, sort alphabetically
+  Listing.find().sort({code:1}).exec(function(error, listings) {
+    if(error) {
+      //Send 400 if error
+      res.status(400).send(error);
+    } else {
+      //Else, Sends a JSON response composed of a stringified
+      //version of the specified data
+      res.json(listings);
+    }});};
 
-/* 
-  Middleware: find a listing by its ID, then pass it to the next request handler. 
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
 
-  HINT: Find the listing using a mongoose query, 
-        bind it to the request object as the property 'listing', 
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
         then finally call next
  */
 exports.listingByID = function(req, res, next, id) {
